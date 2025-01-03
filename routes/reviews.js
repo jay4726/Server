750d4006bfc31de617526db6772b8d60f7a45334
Jayshil Thakkar <76470342+jay4726@users.noreply.github.com>
2025-01-03 12:19:19 +0530

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllReviews, addReview} = require('../controllers/reviews');
+const {getAllReviews, addReview, deleteReview} = require('../controllers/reviews');
 
 router.route('/').get(getAllReviews);
 router.route('/').post(addReview);
+router.delete('/:id', deleteReview);
+
 
 module.exports = router;
-
